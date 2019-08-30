1f4158c04798502ffe71ce7f3e82c12837fc15df
ivan <ivangabor.job@gmail.com>
2019-08-30 20:37:57 +0300

    register models im admin panel

--- trackerbalance/models.py
+++ trackerbalance/models.py
@@ -8,9 +8,15 @@ class Address(models.Model):
     label = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.address
+
 
 class BalanceHistory(models.Model):
     balance = models.DecimalField(max_digits=19, decimal_places=2)
     address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='history')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{}-{}-{}".format(self.address.address, self.balance, self.created_at)
 
